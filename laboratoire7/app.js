e2d69b514e4a7f9a0be62f13d70af5377d55920c
Marc-Antoine Bouchard Marceau <marc-antoine.bm@libeo.com>
2016-03-11 22:29:41 -0500

    Ca me semble logique que lurl soit dans la collection

--- laboratoire7/app.js
+++ laboratoire7/app.js
@@ -1,6 +1,5 @@
 $(function () {
     var dayCollection = new DayCollection({});
-    dayCollection.url = 'http://api.wunderground.com/api/ccf8fe1ec42afad2/forecast7day/q/Québec.json';
 
     var dayView = new DayView({
         collection: dayCollection
--- laboratoire7/js/collections/days.js
+++ laboratoire7/js/collections/days.js
@@ -1,6 +1,7 @@
 $(function() {
     DayCollection = Backbone.Collection.extend({
         model: DayModel,
+        url: 'http://api.wunderground.com/api/ccf8fe1ec42afad2/forecast7day/q/Québec.json',
         parse: function(response) {
             return response.forecast.simpleforecast.forecastday;
         }
