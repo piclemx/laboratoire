14ea5ef70d4c29567795dcbfa38008f4697b7019
Alexandre <alexandre.picardlemieux@gmail.com>
2016-03-15 19:11:07 -0400

    add element

--- laboratoire8/index.js
+++ laboratoire8/index.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var tasks = require('./routes/tasks');
+var mongo = require('mongodb-wrapper');
+var db = mongo.db('localhost', 27017, 'labo8');
 var app = express();
+
+db.collection('tasks');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
 app.get('/tasks', tasks.getTasks);
-app.get('/tasks/:id', tasks.getById);
+app.get('/tasks/:id', tasks.getTaskById);
 app.post('/tasks', tasks.createTask);
 app.put('/tasks/:id', tasks.updateTask);
 app.delete('/tasks/:id', tasks.deleteTask);
---
+++ laboratoire8/routes/tasks.js
@@ -0,0 +1,26 @@
+exports.getTasks = function (req, res) {
+    console.log('getTasks');
+    res.send();
+};
+
+
+exports.getTaskById = function (req, res) {
+  console.log('getTaskById');
+  res.send();
+};
+
+exports.createTask = function (req,res) {
+    console.log('createTask');
+    res.send();
+};
+
+
+exports.updateTask = function (req, res) {
+    console.log('updateTask');
+    res.send();
+};
+
+exports.deleteTask = function (req, res) {
+    console.log('deleteTask');
+    res.send();
+};
