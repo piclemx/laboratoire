0b639589935a2214b5afa0c9af9f033d8d9021f1
Alexandre <alexandre.picardlemieux@gmail.com>
2016-03-15 18:53:57 -0400

    adding routes in the index.js

--- laboratoire8/index.js
+++ laboratoire8/index.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var tasks = require('./routes/tasks');
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
+app.get('/tasks', tasks.getTasks);
+app.get('/tasks/:id', tasks.getById);
+app.post('/tasks', tasks.createTask);
+app.put('/tasks/:id', tasks.updateTask);
+app.delete('/tasks/:id', tasks.deleteTask);
 
 
 var port = process.env.PORT || 3000;
