78cccae4a0b3a29757ac9365310ee171d7746427
Alexandre <alexandre.picardlemieux@gmail.com>
2016-03-15 18:43:12 -0400

    continue the index.js

--- laboratoire8/index.js
+++ laboratoire8/index.js
@@ -1,5 +1,11 @@
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
+var app = express();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+
+
+var port = process.env.PORT || 3000;
+app.listen(port);
